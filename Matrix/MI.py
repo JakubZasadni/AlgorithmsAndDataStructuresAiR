9daee382a8ae0ba73e9f591402d95b83be9ce8a5
Jakub Zasadni <129856604+JakubZasadni@users.noreply.github.com>
2024-08-27 23:41:41 +0200

    Update MI.py

--- Matrix/MI.py
+++ Matrix/MI.py
@@ -1,100 +1,50 @@
-class Node:
-    def __init__(self, data=None, next_node=None):
-        self.data = data
-        self.next_node = next_node
-
-class LinkedList:
-    def __init__(self):
-        self.head = None
-
-    def add(self, data):
-        new_node = Node(data, self.head)
-        self.head = new_node
-
-    def append(self, data):
-        new_node = Node(data)
-        if not self.head:
-            self.head = new_node
+class Macierz:
+    def __init__(self, arg, fill_value=0):
+        if isinstance(arg, tuple):
+            self.rows, self.cols = arg
+            self.data = [[fill_value] * self.cols for _ in range(self.rows)]
         else:
-            current = self.head
-            while current.next_node:
-                current = current.next_node
-            current.next_node = new_node
+            self.data = arg
+            self.rows, self.cols = len(arg), len(arg[0])
 
-    def remove(self):
-        if self.head:
-            self.head = self.head.next_node
+    def __add__(self, other):
+        if self.size() != other.size():
+            raise ValueError("Wymiary macierzy musza sie zgadzac")
+        return Macierz([[a + b for a, b in zip(row1, row2)] for row1, row2 in zip(self.data, other.data)])
 
-    def remove_end(self):
-        if not self.head or not self.head.next_node:
-            self.head = None
-        else:
-            current = self.head
-            prev = None
-            while current.next_node:
-                prev = current
-                current = current.next_node
-            prev.next_node = None
+    def __mul__(self, other):
+        if self.cols != other.rows:
+            raise ValueError("Liczba kolumn musi być równa liczbie wierszy")
+        result = [[sum(x * y for x, y in zip(row, col)) for col in zip(*other.data)] for row in self.data]
+        return Macierz(result)
 
-    def is_empty(self):
-        return not self.head
-
-    def length(self):
-        count = 0
-        current = self.head
-        while current:
-            count += 1
-            current = current.next_node
-        return count
-
-    def get(self):
-        return self.head.data if self.head else None
-
-    def destroy(self):
-        self.head = None
+    def __getitem__(self, index):
+        return self.data[index]
 
     def __str__(self):
-        result = []
-        current = self.head
-        while current:
-            result.append(str(current.data))
-            current = current.next_node
-        return "\n".join(result)
+        return "\n".join("| " + "   ".join(map(str, row)) + " |" for row in self.data)
+
+    def size(self):
+        return self.rows, self.cols
 
 
-def main():
-    universities_data = [
-        ('AGH', 'Kraków', 1919),
-        ('UJ', 'Kraków', 1364),
-        ('PW', 'Warszawa', 1915),
-        ('UW', 'Warszawa', 1915),
-        ('UP', 'Poznań', 1919),
-        ('PG', 'Gdańsk', 1945)
-    ]
-
-    uczelnie = LinkedList()
-
-    for i in range(3):
-        uczelnie.append(universities_data[i])
-    for i in range(3, 6):
-        uczelnie.add(universities_data[i])
-
-    print(uczelnie)
-    print(uczelnie.length())
-    print()
-    uczelnie.remove()
-    print(uczelnie.get())
-    print()
-    uczelnie.remove_end()
-    print(uczelnie)
-    uczelnie.destroy()
-    print(uczelnie.is_empty())
-    uczelnie.remove()
-    uczelnie.append(universities_data[0])
-    uczelnie.remove_end()
-    print(uczelnie)
-    print(uczelnie.is_empty())
+def transpose(matrix):
+    return Macierz(list(map(list, zip(*matrix))))
 
 
 if __name__ == "__main__":
-    main()
+    m1 = Macierz([[1, 0, 2], [-1, 3, 1]])
+    print(m1, '\n')
+
+    transposed_matrix = transpose(m1)
+    print(transposed_matrix, '\n')
+
+    m2 = Macierz((2, 3), fill_value=1)
+    print(m2)
+
+    sum_matrix = m1 + m2
+    print(sum_matrix, '\n')
+
+    m3 = Macierz([[3, 1], [2, 1], [1, 0]])
+    product_matrix = m1 * m3
+    print(product_matrix)
